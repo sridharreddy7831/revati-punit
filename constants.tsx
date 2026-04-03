09d12b896b71883715006c60ba4d0056fc6a310a
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2026-04-04 00:09:43 +0530

    v2

--- constants.tsx
+++ constants.tsx
@@ -40,22 +40,15 @@ export const SLOKAS = [
 ];
 
 export const HORIZONTAL_IMAGES = [
-  { url: '/6.jpg' },
-  { url: '/1.jpg' },
   { url: '/2.jpg' },
   { url: '/11.jpg' },
-  { url: '/9.jpg' },
-  { url: '/12.jpg' },
-  { url: '/10.jpg' },
   { url: '/1.jpg' },
-  { url: '/2.jpg' },
 ];
 
 export const VERTICAL_IMAGES = [
   { url: '/7.jpg' },
   { url: '/3.jpg' },
   { url: '/5.jpg' },
-  { url: '/4.jpg' },
   { url: '/13.jpg' },
 ];
 
